2e441e9c164356e1cd9f33e7bdf7f812e6a694fa
Areej <Areej-fahad>
2022-11-06 09:51:19 +0300

    Update

--- src/Component/Img.js
+++ src/Component/Img.js
@@ -2,30 +2,31 @@ import axios  from "axios";
 import 'semantic-ui-css/semantic.min.css'
 import { useState,useEffect } from "react";
 import React from 'react'
+import {Image} from '@chakra-ui/react'
 
 
 function Img() {
-    const Arr = [{}];   
+// const [item , setItem] = useState([]);
 
-    const [get,setget] = useState ("");
+    const [get,setget] = useState ([]);
     useEffect ( () => {
-        axios.get('https://api.themoviedb.org/3/discover/movie?api_key=2d6b3291586411f85a61201ca446cbb8&with_genres=28') 
+        axios.get('https://api.themoviedb.org/3/discover/movie?api_key=2d6b3291586411f85a61201ca446cbb8&with_genres=16') 
         .then ( (res) => {
             setget(res.data.results);
-          console.log(res);
+          console.log(res.data.results);
         })
       }, []);
       return (
         <div className='cards'>
-        {Arr.map ( (item) => {
+        {get.map ( (item) => {
           return (
             
               <div className='Container'>
                 <div className='image-container'>
-                  <image key={item.id} src={ `https://image.tmdb.org/t/p/w500` } />
+                  <Image src={`https://image.tmdb.org/t/p/w500${item.poster_path}` } />
                 </div>
                 <div className='title-container'>
-                  <h2   > {item.backdrop_path}</h2></div>
+                  <h2   > {item.original_title}</h2></div>
                 <div className='btn-contener'>
                   <button >Delete</button>
                   <button >Update</button>
